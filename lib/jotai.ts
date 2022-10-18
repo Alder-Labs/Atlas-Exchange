b595a41fa82d73f0b1b22c5324fb32318a3aeff3
Max Wu <chengmaxwu@gmail.com>
2022-10-18 13:34:22 -0700

    Watch balance for a few seconds after ach deposit

--- lib/jotai.ts
+++ lib/jotai.ts
@@ -13,3 +13,12 @@ export const globalModalStateDetailedAtom = atom<ModalStateDetailed>({
 export const globalModalStateDetailedStackAtom = atom<ModalStateDetailed[]>([]);
 
 export const sardineDeviceIdAtom = atom<string | undefined>(undefined);
+
+/**
+ * Sometimes, after a user does something that might change their balance,
+ * we want to continuously refetch their balance for a few seconds.
+ *
+ * If the current time is less than this atom's value, we will refetch the
+ * user's balance every 2 seconds. (in the useBalances hook)
+ */
+export const watchBalanceUntilAtom = atom<number>(0);
