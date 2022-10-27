446c523b30b0124255c78be3efa473c7100b0f48
Max Wu <chengmaxwu@gmail.com>
2022-10-27 12:03:44 -0700

    Keep watchlist on sign out

--- lib/local-storage-keys.ts
+++ lib/local-storage-keys.ts
@@ -1,8 +1,14 @@
 export enum LocalStorageKey {
+  // Used in auth context
   User = 'userObject',
   TokenDate = 'tokenDate',
+
+  // Store kyc form data between refreshes
   KycForm = 'kycForm',
+
+  // Market watchlist (store which tokens to watch)
+  WatchList = 'user_asset_watchlist',
+
   SardineSdkConfig = 'sardineSdkConfig',
   DarkModeTheme = 'darkModeTheme',
-  WatchList = 'user_asset_watchlist',
 }
