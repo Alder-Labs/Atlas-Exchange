7c5e92c1991b16ae10d80be45a2d17e46cd34f17
Jeff Zhao <jeff.no.zhao@gmail.com>
2022-11-01 20:47:54 -0400

    fix typo

--- components/mfa/WithdrawalPasswordWarning.tsx
+++ components/mfa/WithdrawalPasswordWarning.tsx
@@ -5,7 +5,7 @@ export function WithdrawalPasswordWarning(props: { onClick: () => void }) {
     <div>
       <Text>
         For 24 hours after you change or disable your withdrawal password, you
-        will not be able to withdraw funds from your account.This is for your
+        will not be able to withdraw funds from your account. This is for your
         account&apos;s security.
       </Text>
       <div className="h-6" />
